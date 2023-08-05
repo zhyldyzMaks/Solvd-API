3e610f350bb8e523211db64b6ffa32922421f867
zhyldyzmaksatbek <jyldyzmbek@gmail.com>
2023-08-04 20:30:43 -0500

    Changes in Response Annotation.

--- src/main/java/com/solvd/api/youtube/GetVideoCommentsMethod.java
+++ src/main/java/com/solvd/api/youtube/GetVideoCommentsMethod.java
@@ -2,7 +2,6 @@ package com.solvd.api.youtube;
 
 import com.zebrunner.carina.api.AbstractApiMethodV2;
 import com.zebrunner.carina.api.annotation.Endpoint;
-import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
 import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
 import com.zebrunner.carina.api.http.HttpMethodType;
 import com.zebrunner.carina.api.http.HttpResponseStatusType;
@@ -11,7 +10,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Endpoint(url = "${base_url}/commentThreads", methodType = HttpMethodType.GET)
-@ResponseTemplatePath(path = "api/youtube/_get/comments_rs.json")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class GetVideoCommentsMethod extends AbstractApiMethodV2 {
     private static final Logger logger = LogManager.getLogger(GetVideoCommentsMethod.class);
--- src/main/java/com/solvd/api/youtube/SearchVideosMethod.java
+++ src/main/java/com/solvd/api/youtube/SearchVideosMethod.java
@@ -2,12 +2,14 @@ package com.solvd.api.youtube;
 
 import com.zebrunner.carina.api.AbstractApiMethodV2;
 import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
 import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
 import com.zebrunner.carina.api.http.HttpMethodType;
 import com.zebrunner.carina.api.http.HttpResponseStatusType;
 import com.zebrunner.carina.utils.config.Configuration;
 
 @Endpoint(url = "${base_url}/search", methodType = HttpMethodType.GET)
+@ResponseTemplatePath(path = "src/test/resources/api/youtube/_get/search_rs.json")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class SearchVideosMethod extends AbstractApiMethodV2 {
 
