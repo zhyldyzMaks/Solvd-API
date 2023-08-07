e2a6cce4de053810b82b7606ba968b0d7ebc3e55
zhyldyzmaksatbek <jyldyzmbek@gmail.com>
2023-08-07 07:59:35 -0500

    Removed unused line of code

--- src/main/java/com/solvd/api/youtube/GetVideoCommentsMethod.java
+++ src/main/java/com/solvd/api/youtube/GetVideoCommentsMethod.java
@@ -6,13 +6,10 @@ import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
 import com.zebrunner.carina.api.http.HttpMethodType;
 import com.zebrunner.carina.api.http.HttpResponseStatusType;
 import com.zebrunner.carina.utils.config.Configuration;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Endpoint(url = "${base_url}/commentThreads", methodType = HttpMethodType.GET)
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class GetVideoCommentsMethod extends AbstractApiMethodV2 {
-    private static final Logger logger = LogManager.getLogger(GetVideoCommentsMethod.class);
 
     public GetVideoCommentsMethod(String videoId) {
         replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
